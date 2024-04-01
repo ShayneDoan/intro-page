58fea8ebf7c781034571bde98b4364856d943f95
Shayaan <shayaansufi@gmail.com>
2024-04-01 15:51:02 -0400

    Add animation for fading in header

--- main.js
+++ main.js
@@ -15,5 +15,26 @@ menuBtn.addEventListener("click", (e) => {
 navLinks.addEventListener("click", (e) => {
     navLinks.classList.remove("open")
     menuBtnIcon.setAttribute("class", "ri-menu-3-line");
-})
+});
+
+const scrollRevealOption = {
+    distance:"50px",
+    origin:"bottom",
+    duration:1000,
+};
+
+//header container 
+ScrollReveal().reveal(".header_content h1", {
+    ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".header_content .section_description", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".header_content .header_btn", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
 
